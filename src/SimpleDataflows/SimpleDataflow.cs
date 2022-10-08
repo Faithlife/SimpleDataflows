6509df5324d3a0af376f462714d215332697e359
Ed Ball <ed.ball@faithlife.com>
2022-10-08 12:32:52 -0700

    Use file-scoped namespace.

--- src/SimpleDataflows/SimpleDataflow.cs
+++ src/SimpleDataflows/SimpleDataflow.cs
@@ -5,295 +5,294 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
-namespace SimpleDataflows
+namespace SimpleDataflows;
+
+/// <summary>
+/// Supports a linear pipeline of data using TPL Dataflow.
+/// </summary>
+public static class SimpleDataflow
 {
 	/// <summary>
-	/// Supports a linear pipeline of data using TPL Dataflow.
+	/// Starts a linear pipeline of data using TPL Dataflow.
 	/// </summary>
-	public static class SimpleDataflow
+	public static SimpleDataflow<ValueTuple> Create(CancellationToken cancellationToken = default)
 	{
-		/// <summary>
-		/// Starts a linear pipeline of data using TPL Dataflow.
-		/// </summary>
-		public static SimpleDataflow<ValueTuple> Create(CancellationToken cancellationToken = default)
-		{
-			var input = new TransformBlock<ValueTuple, ValueTuple>(x => x);
-			return new SimpleDataflow<ValueTuple>(input, input, CreateDefaultBlockOptions(cancellationToken), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
-		}
-
-		/// <summary>
-		/// Starts a linear pipeline of data using TPL Dataflow.
-		/// </summary>
-		public static SimpleDataflow<T> Create<T>(IEnumerable<T> initial, CancellationToken cancellationToken = default)
-		{
-			var input = new TransformManyBlock<ValueTuple, T>(_ => initial);
-			return new SimpleDataflow<T>(input, input, CreateDefaultBlockOptions(cancellationToken), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
-		}
-
-		internal static readonly DataflowLinkOptions DataflowLinkOptions = new() { PropagateCompletion = true };
-
-		private static ExecutionDataflowBlockOptions CreateDefaultBlockOptions(CancellationToken cancellationToken) =>
-			new()
-			{
-				BoundedCapacity = DataflowBlockOptions.Unbounded,
-				CancellationToken = cancellationToken,
-				EnsureOrdered = false,
-				MaxDegreeOfParallelism = Math.Max(1, Environment.ProcessorCount / 2),
-			};
+		var input = new TransformBlock<ValueTuple, ValueTuple>(x => x);
+		return new SimpleDataflow<ValueTuple>(input, input, CreateDefaultBlockOptions(cancellationToken), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
 	}
 
 	/// <summary>
-	/// A linear pipeline of data using TPL dataflow.
+	/// Starts a linear pipeline of data using TPL Dataflow.
 	/// </summary>
-	[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "generic type")]
-	public sealed class SimpleDataflow<T>
+	public static SimpleDataflow<T> Create<T>(IEnumerable<T> initial, CancellationToken cancellationToken = default)
 	{
-		/// <summary>
-		/// Links a <c>TransformBlock</c> to the pipeline.
-		/// </summary>
-		public SimpleDataflow<TNext> Transform<TNext>(Func<T, TNext> func) =>
-			LinkTo(new TransformBlock<T, TNext>(value =>
-			{
-				try
-				{
-					return func(value);
-				}
-				catch (Exception) when (CancelAndRethrow())
-				{
-					throw;
-				}
-			}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Links a <c>TransformBlock</c> to the pipeline.
-		/// </summary>
-		public SimpleDataflow<TNext> Transform<TNext>(Func<T, Task<TNext>> func) =>
-			LinkTo(new TransformBlock<T, TNext>(
-				async value =>
-				{
-					try
-					{
-						return await func(value).ConfigureAwait(false);
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Links a <c>TransformBlock</c> to the pipeline.
-		/// </summary>
-		public SimpleDataflow<TNext> Transform<TNext>(Func<T, CancellationToken, Task<TNext>> func)
-		{
-			var cancellationToken = m_cancellationTokenSource.Token;
-			return LinkTo(new TransformBlock<T, TNext>(
-				async value =>
-				{
-					try
-					{
-						return await func(value, cancellationToken).ConfigureAwait(false);
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-		}
-
-		/// <summary>
-		/// Links a <c>TransformManyBlock</c> to the pipeline.
-		/// </summary>
-		public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, IEnumerable<TNext>> func) =>
-			LinkTo(new TransformManyBlock<T, TNext>(value =>
-			{
-				try
-				{
-					return func(value);
-				}
-				catch (Exception) when (CancelAndRethrow())
-				{
-					throw;
-				}
-			}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Links a <c>TransformManyBlock</c> to the pipeline.
-		/// </summary>
-		/// <remarks>If necessary, use <c>Enumerable.AsEnumerable</c> to cast the return value to an
-		/// <c>IEnumerable&lt;T&gt;</c> when using a lambda expression.</remarks>
-		public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, Task<IEnumerable<TNext>>> func) =>
-			LinkTo(new TransformManyBlock<T, TNext>(
-				async value =>
-				{
-					try
-					{
-						return await func(value).ConfigureAwait(false);
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Links a <c>TransformManyBlock</c> to the pipeline.
-		/// </summary>
-		/// <remarks>If necessary, use <c>Enumerable.AsEnumerable</c> to cast the return value to an
-		/// <c>IEnumerable&lt;T&gt;</c> when using a lambda expression.</remarks>
-		public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, CancellationToken, Task<IEnumerable<TNext>>> func)
-		{
-			var cancellationToken = m_cancellationTokenSource.Token;
-			return LinkTo(new TransformManyBlock<T, TNext>(
-				async value =>
-				{
-					try
-					{
-						return await func(value, cancellationToken).ConfigureAwait(false);
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-		}
-
-		/// <summary>
-		/// Links a <c>BatchBlock</c> to the pipeline.
-		/// </summary>
-		public SimpleDataflow<T[]> Batch(int batchSize) =>
-			LinkTo(new BatchBlock<T>(batchSize));
-
-		/// <summary>
-		/// Executes the specified action on each item in the pipeline.
-		/// </summary>
-		/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
-		public SimpleDataflow<T> ForAll(Action<T> action) =>
-			LinkTo(new TransformBlock<T, T>(
-				value =>
-				{
-					try
-					{
-						action(value);
-						return value;
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Executes the specified action on each item in the pipeline.
-		/// </summary>
-		/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
-		public SimpleDataflow<T> ForAll(Func<T, Task> action) =>
-			LinkTo(new TransformBlock<T, T>(
-				async value =>
-				{
-					try
-					{
-						await action(value).ConfigureAwait(false);
-						return value;
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-
-		/// <summary>
-		/// Executes the specified action on each item in the pipeline.
-		/// </summary>
-		/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
-		public SimpleDataflow<T> ForAll(Func<T, CancellationToken, Task> action)
-		{
-			var cancellationToken = m_cancellationTokenSource.Token;
-			return LinkTo(new TransformBlock<T, T>(
-				async value =>
-				{
-					try
-					{
-						await action(value, cancellationToken).ConfigureAwait(false);
-						return value;
-					}
-					catch (Exception) when (CancelAndRethrow())
-					{
-						throw;
-					}
-				}, m_nextBlockOptions));
-		}
-
-		/// <summary>
-		/// Links the specified block to the pipeline.
-		/// </summary>
-		public SimpleDataflow<TNext> LinkTo<TNext>(IPropagatorBlock<T, TNext> newOutput)
-		{
-			m_output.LinkTo(newOutput, SimpleDataflow.DataflowLinkOptions);
-			return new SimpleDataflow<TNext>(m_input, newOutput, m_nextBlockOptions, m_cancellationTokenSource);
-		}
-
-		/// <summary>
-		/// Sets the bounded capacity for the next blocks.
-		/// </summary>
-		/// <remarks>Use this setting to avoid running out of memory while earlier blocks wait for later blocks.
-		/// If this method is not called, the default is <c>DataflowBlockOptions.Unbounded</c>.</remarks>
-		public SimpleDataflow<T> BoundedCapacity(int value)
-		{
-			m_nextBlockOptions.BoundedCapacity = value;
-			return this;
-		}
-
-		/// <summary>
-		/// Sets ordered processing for the next blocks.
-		/// </summary>
-		/// <remarks>If this method is not called, the default is unordered.</remarks>
-		public SimpleDataflow<T> EnsureOrdered(bool value = true)
-		{
-			m_nextBlockOptions.EnsureOrdered = value;
-			return this;
-		}
-
-		/// <summary>
-		/// Sets the maximum degree of parallelism for the next blocks.
-		/// </summary>
-		/// <remarks>If this method is not called, the default is <c>Environment.ProcessorCount / 2</c>.</remarks>
-		public SimpleDataflow<T> MaxDegreeOfParallelism(int value)
-		{
-			m_nextBlockOptions.MaxDegreeOfParallelism = value;
-			return this;
-		}
-
-		/// <summary>
-		/// Executes the pipeline.
-		/// </summary>
-		public async Task ExecuteAsync()
-		{
-			m_output.LinkTo(DataflowBlock.NullTarget<T>());
-			if (!await m_input.SendAsync(default).ConfigureAwait(false))
-				throw new InvalidOperationException("Input rejected.");
-			m_input.Complete();
-			await m_output.Completion.ConfigureAwait(false);
-			m_cancellationTokenSource.Dispose();
-		}
-
-		private bool CancelAndRethrow()
-		{
-			m_cancellationTokenSource.Cancel();
-			return false;
-		}
-
-		internal SimpleDataflow(ITargetBlock<ValueTuple> input, ISourceBlock<T> output, ExecutionDataflowBlockOptions nextBlockOptions, CancellationTokenSource cancellationTokenSource)
-		{
-			m_input = input;
-			m_output = output;
-			m_nextBlockOptions = nextBlockOptions;
-			m_cancellationTokenSource = cancellationTokenSource;
-		}
-
-		private readonly ITargetBlock<ValueTuple> m_input;
-		private readonly ISourceBlock<T> m_output;
-		private readonly ExecutionDataflowBlockOptions m_nextBlockOptions;
-		private readonly CancellationTokenSource m_cancellationTokenSource;
+		var input = new TransformManyBlock<ValueTuple, T>(_ => initial);
+		return new SimpleDataflow<T>(input, input, CreateDefaultBlockOptions(cancellationToken), CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
 	}
+
+	internal static readonly DataflowLinkOptions DataflowLinkOptions = new() { PropagateCompletion = true };
+
+	private static ExecutionDataflowBlockOptions CreateDefaultBlockOptions(CancellationToken cancellationToken) =>
+		new()
+		{
+			BoundedCapacity = DataflowBlockOptions.Unbounded,
+			CancellationToken = cancellationToken,
+			EnsureOrdered = false,
+			MaxDegreeOfParallelism = Math.Max(1, Environment.ProcessorCount / 2),
+		};
+}
+
+/// <summary>
+/// A linear pipeline of data using TPL dataflow.
+/// </summary>
+[SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "generic type")]
+public sealed class SimpleDataflow<T>
+{
+	/// <summary>
+	/// Links a <c>TransformBlock</c> to the pipeline.
+	/// </summary>
+	public SimpleDataflow<TNext> Transform<TNext>(Func<T, TNext> func) =>
+		LinkTo(new TransformBlock<T, TNext>(value =>
+		{
+			try
+			{
+				return func(value);
+			}
+			catch (Exception) when (CancelAndRethrow())
+			{
+				throw;
+			}
+		}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Links a <c>TransformBlock</c> to the pipeline.
+	/// </summary>
+	public SimpleDataflow<TNext> Transform<TNext>(Func<T, Task<TNext>> func) =>
+		LinkTo(new TransformBlock<T, TNext>(
+			async value =>
+			{
+				try
+				{
+					return await func(value).ConfigureAwait(false);
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Links a <c>TransformBlock</c> to the pipeline.
+	/// </summary>
+	public SimpleDataflow<TNext> Transform<TNext>(Func<T, CancellationToken, Task<TNext>> func)
+	{
+		var cancellationToken = m_cancellationTokenSource.Token;
+		return LinkTo(new TransformBlock<T, TNext>(
+			async value =>
+			{
+				try
+				{
+					return await func(value, cancellationToken).ConfigureAwait(false);
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+	}
+
+	/// <summary>
+	/// Links a <c>TransformManyBlock</c> to the pipeline.
+	/// </summary>
+	public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, IEnumerable<TNext>> func) =>
+		LinkTo(new TransformManyBlock<T, TNext>(value =>
+		{
+			try
+			{
+				return func(value);
+			}
+			catch (Exception) when (CancelAndRethrow())
+			{
+				throw;
+			}
+		}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Links a <c>TransformManyBlock</c> to the pipeline.
+	/// </summary>
+	/// <remarks>If necessary, use <c>Enumerable.AsEnumerable</c> to cast the return value to an
+	/// <c>IEnumerable&lt;T&gt;</c> when using a lambda expression.</remarks>
+	public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, Task<IEnumerable<TNext>>> func) =>
+		LinkTo(new TransformManyBlock<T, TNext>(
+			async value =>
+			{
+				try
+				{
+					return await func(value).ConfigureAwait(false);
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Links a <c>TransformManyBlock</c> to the pipeline.
+	/// </summary>
+	/// <remarks>If necessary, use <c>Enumerable.AsEnumerable</c> to cast the return value to an
+	/// <c>IEnumerable&lt;T&gt;</c> when using a lambda expression.</remarks>
+	public SimpleDataflow<TNext> TransformMany<TNext>(Func<T, CancellationToken, Task<IEnumerable<TNext>>> func)
+	{
+		var cancellationToken = m_cancellationTokenSource.Token;
+		return LinkTo(new TransformManyBlock<T, TNext>(
+			async value =>
+			{
+				try
+				{
+					return await func(value, cancellationToken).ConfigureAwait(false);
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+	}
+
+	/// <summary>
+	/// Links a <c>BatchBlock</c> to the pipeline.
+	/// </summary>
+	public SimpleDataflow<T[]> Batch(int batchSize) =>
+		LinkTo(new BatchBlock<T>(batchSize));
+
+	/// <summary>
+	/// Executes the specified action on each item in the pipeline.
+	/// </summary>
+	/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
+	public SimpleDataflow<T> ForAll(Action<T> action) =>
+		LinkTo(new TransformBlock<T, T>(
+			value =>
+			{
+				try
+				{
+					action(value);
+					return value;
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Executes the specified action on each item in the pipeline.
+	/// </summary>
+	/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
+	public SimpleDataflow<T> ForAll(Func<T, Task> action) =>
+		LinkTo(new TransformBlock<T, T>(
+			async value =>
+			{
+				try
+				{
+					await action(value).ConfigureAwait(false);
+					return value;
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+
+	/// <summary>
+	/// Executes the specified action on each item in the pipeline.
+	/// </summary>
+	/// <remarks>Implemented with a <c>TransformBlock</c> that returns the same items.</remarks>
+	public SimpleDataflow<T> ForAll(Func<T, CancellationToken, Task> action)
+	{
+		var cancellationToken = m_cancellationTokenSource.Token;
+		return LinkTo(new TransformBlock<T, T>(
+			async value =>
+			{
+				try
+				{
+					await action(value, cancellationToken).ConfigureAwait(false);
+					return value;
+				}
+				catch (Exception) when (CancelAndRethrow())
+				{
+					throw;
+				}
+			}, m_nextBlockOptions));
+	}
+
+	/// <summary>
+	/// Links the specified block to the pipeline.
+	/// </summary>
+	public SimpleDataflow<TNext> LinkTo<TNext>(IPropagatorBlock<T, TNext> newOutput)
+	{
+		m_output.LinkTo(newOutput, SimpleDataflow.DataflowLinkOptions);
+		return new SimpleDataflow<TNext>(m_input, newOutput, m_nextBlockOptions, m_cancellationTokenSource);
+	}
+
+	/// <summary>
+	/// Sets the bounded capacity for the next blocks.
+	/// </summary>
+	/// <remarks>Use this setting to avoid running out of memory while earlier blocks wait for later blocks.
+	/// If this method is not called, the default is <c>DataflowBlockOptions.Unbounded</c>.</remarks>
+	public SimpleDataflow<T> BoundedCapacity(int value)
+	{
+		m_nextBlockOptions.BoundedCapacity = value;
+		return this;
+	}
+
+	/// <summary>
+	/// Sets ordered processing for the next blocks.
+	/// </summary>
+	/// <remarks>If this method is not called, the default is unordered.</remarks>
+	public SimpleDataflow<T> EnsureOrdered(bool value = true)
+	{
+		m_nextBlockOptions.EnsureOrdered = value;
+		return this;
+	}
+
+	/// <summary>
+	/// Sets the maximum degree of parallelism for the next blocks.
+	/// </summary>
+	/// <remarks>If this method is not called, the default is <c>Environment.ProcessorCount / 2</c>.</remarks>
+	public SimpleDataflow<T> MaxDegreeOfParallelism(int value)
+	{
+		m_nextBlockOptions.MaxDegreeOfParallelism = value;
+		return this;
+	}
+
+	/// <summary>
+	/// Executes the pipeline.
+	/// </summary>
+	public async Task ExecuteAsync()
+	{
+		m_output.LinkTo(DataflowBlock.NullTarget<T>());
+		if (!await m_input.SendAsync(default).ConfigureAwait(false))
+			throw new InvalidOperationException("Input rejected.");
+		m_input.Complete();
+		await m_output.Completion.ConfigureAwait(false);
+		m_cancellationTokenSource.Dispose();
+	}
+
+	private bool CancelAndRethrow()
+	{
+		m_cancellationTokenSource.Cancel();
+		return false;
+	}
+
+	internal SimpleDataflow(ITargetBlock<ValueTuple> input, ISourceBlock<T> output, ExecutionDataflowBlockOptions nextBlockOptions, CancellationTokenSource cancellationTokenSource)
+	{
+		m_input = input;
+		m_output = output;
+		m_nextBlockOptions = nextBlockOptions;
+		m_cancellationTokenSource = cancellationTokenSource;
+	}
+
+	private readonly ITargetBlock<ValueTuple> m_input;
+	private readonly ISourceBlock<T> m_output;
+	private readonly ExecutionDataflowBlockOptions m_nextBlockOptions;
+	private readonly CancellationTokenSource m_cancellationTokenSource;
 }
--- tests/SimpleDataflows.Tests/SimpleDataflowTests.cs
+++ tests/SimpleDataflows.Tests/SimpleDataflowTests.cs
@@ -6,137 +6,136 @@ using System.Threading;
 using System.Threading.Tasks;
 using NUnit.Framework;
 
-namespace SimpleDataflows.Tests
+namespace SimpleDataflows.Tests;
+
+public class SimpleDataflowTests
 {
-	public class SimpleDataflowTests
+	[Test]
+	public async Task Success()
 	{
-		[Test]
-		public async Task Success()
-		{
-			var list = new ConcurrentBag<int>();
-			await SimpleDataflow.Create(new[] { 1, 10, 100 })
-				.TransformMany(x => Enumerable.Range(1, x))
-				.Batch(10)
-				.Transform(async x => await Task.Run(x.Sum))
-				.Batch(int.MaxValue)
-				.Transform(x => x.Sum())
-				.ForAll(x => list.Add(x))
-				.ExecuteAsync();
-			CollectionAssert.AreEqual(new[] { 1 + 55 + 5050 }, list);
-		}
+		var list = new ConcurrentBag<int>();
+		await SimpleDataflow.Create(new[] { 1, 10, 100 })
+			.TransformMany(x => Enumerable.Range(1, x))
+			.Batch(10)
+			.Transform(async x => await Task.Run(x.Sum))
+			.Batch(int.MaxValue)
+			.Transform(x => x.Sum())
+			.ForAll(x => list.Add(x))
+			.ExecuteAsync();
+		CollectionAssert.AreEqual(new[] { 1 + 55 + 5050 }, list);
+	}
 
-		[Test]
-		public async Task VoidStart()
+	[Test]
+	public async Task VoidStart()
+	{
+		var list = new ConcurrentBag<int>();
+		await SimpleDataflow.Create()
+			.TransformMany(_ => Enumerable.Range(1, 100))
+			.Batch(10)
+			.Transform(async x => await Task.Run(x.Sum))
+			.Batch(int.MaxValue)
+			.Transform(x => x.Sum())
+			.ForAll(x => list.Add(x))
+			.ExecuteAsync();
+		CollectionAssert.AreEqual(new[] { 5050 }, list);
+	}
+
+	[Test]
+	public async Task BoundedCapacity([Values(1, 2)] int value)
+	{
+		var monitor = new object();
+		var running = 0;
+		var maxRunning = 0;
+		await SimpleDataflow.Create(Enumerable.Range(0, 4))
+			.BoundedCapacity(value)
+			.Transform(x =>
+			{
+				lock (monitor)
+					running++;
+				Thread.Sleep(1000);
+				lock (monitor)
+					maxRunning = Math.Max(running--, maxRunning);
+				return x;
+			})
+			.ExecuteAsync();
+		Assert.LessOrEqual(maxRunning, value);
+	}
+
+	[Test]
+	public async Task EnsureOrdered()
+	{
+		var list = new List<int>();
+		await SimpleDataflow.Create(new[] { 2000, 0 })
+			.EnsureOrdered()
+			.Transform(async x =>
+			{
+				await Task.Delay(x);
+				return x;
+			})
+			.MaxDegreeOfParallelism(1)
+			.ForAll(x => list.Add(x))
+			.ExecuteAsync();
+		CollectionAssert.AreEqual(new[] { 2000, 0 }, list);
+	}
+
+	[Test]
+	public async Task MaxDegreeOfParallelism([Values(1, 2)] int value)
+	{
+		var monitor = new object();
+		var running = 0;
+		var maxRunning = 0;
+		await SimpleDataflow.Create(Enumerable.Range(0, 4))
+			.MaxDegreeOfParallelism(value)
+			.Transform(x =>
+			{
+				lock (monitor)
+					running++;
+				Thread.Sleep(1000);
+				lock (monitor)
+					maxRunning = Math.Max(running--, maxRunning);
+				return x;
+			})
+			.ExecuteAsync();
+		Assert.AreEqual(value, maxRunning);
+	}
+
+	[Test]
+	public async Task Exception()
+	{
+		try
 		{
-			var list = new ConcurrentBag<int>();
 			await SimpleDataflow.Create()
-				.TransformMany(_ => Enumerable.Range(1, 100))
-				.Batch(10)
-				.Transform(async x => await Task.Run(x.Sum))
-				.Batch(int.MaxValue)
-				.Transform(x => x.Sum())
-				.ForAll(x => list.Add(x))
+				.ForAll(_ => throw new InvalidOperationException())
 				.ExecuteAsync();
-			CollectionAssert.AreEqual(new[] { 5050 }, list);
+			Assert.Fail();
 		}
-
-		[Test]
-		public async Task BoundedCapacity([Values(1, 2)] int value)
+		catch (InvalidOperationException)
 		{
-			var monitor = new object();
-			var running = 0;
-			var maxRunning = 0;
-			await SimpleDataflow.Create(Enumerable.Range(0, 4))
-				.BoundedCapacity(value)
-				.Transform(x =>
-				{
-					lock (monitor)
-						running++;
-					Thread.Sleep(1000);
-					lock (monitor)
-						maxRunning = Math.Max(running--, maxRunning);
-					return x;
-				})
+		}
+	}
+
+	[Test]
+	public async Task Cancelled()
+	{
+		using var cts = new CancellationTokenSource();
+		cts.Cancel();
+		try
+		{
+			await SimpleDataflow.Create(cts.Token)
+				.ForAll(_ => throw new InvalidOperationException())
 				.ExecuteAsync();
-			Assert.LessOrEqual(maxRunning, value);
+			Assert.Fail();
 		}
-
-		[Test]
-		public async Task EnsureOrdered()
+		catch (OperationCanceledException)
 		{
-			var list = new List<int>();
-			await SimpleDataflow.Create(new[] { 2000, 0 })
-				.EnsureOrdered()
-				.Transform(async x =>
-				{
-					await Task.Delay(x);
-					return x;
-				})
-				.MaxDegreeOfParallelism(1)
-				.ForAll(x => list.Add(x))
-				.ExecuteAsync();
-			CollectionAssert.AreEqual(new[] { 2000, 0 }, list);
 		}
+	}
 
-		[Test]
-		public async Task MaxDegreeOfParallelism([Values(1, 2)] int value)
-		{
-			var monitor = new object();
-			var running = 0;
-			var maxRunning = 0;
-			await SimpleDataflow.Create(Enumerable.Range(0, 4))
-				.MaxDegreeOfParallelism(value)
-				.Transform(x =>
-				{
-					lock (monitor)
-						running++;
-					Thread.Sleep(1000);
-					lock (monitor)
-						maxRunning = Math.Max(running--, maxRunning);
-					return x;
-				})
-				.ExecuteAsync();
-			Assert.AreEqual(value, maxRunning);
-		}
-
-		[Test]
-		public async Task Exception()
-		{
-			try
-			{
-				await SimpleDataflow.Create()
-					.ForAll(_ => throw new InvalidOperationException())
-					.ExecuteAsync();
-				Assert.Fail();
-			}
-			catch (InvalidOperationException)
-			{
-			}
-		}
-
-		[Test]
-		public async Task Cancelled()
-		{
-			using var cts = new CancellationTokenSource();
-			cts.Cancel();
-			try
-			{
-				await SimpleDataflow.Create(cts.Token)
-					.ForAll(_ => throw new InvalidOperationException())
-					.ExecuteAsync();
-				Assert.Fail();
-			}
-			catch (OperationCanceledException)
-			{
-			}
-		}
-
-		[Test]
-		public async Task ExecuteTwice()
-		{
-			var dataflow = SimpleDataflow.Create();
-			await dataflow.ExecuteAsync();
-			Assert.ThrowsAsync<InvalidOperationException>(dataflow.ExecuteAsync);
-		}
+	[Test]
+	public async Task ExecuteTwice()
+	{
+		var dataflow = SimpleDataflow.Create();
+		await dataflow.ExecuteAsync();
+		Assert.ThrowsAsync<InvalidOperationException>(dataflow.ExecuteAsync);
 	}
 }
